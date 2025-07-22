1dceed87c5ea17a0ba901d5cf4b0d834a86f7c5b
aamansingh264 <amannsingh264@gmail.com>
2025-07-22 09:10:23 +0530

    Beginning with Javascript Fundamental

---
+++ variable.js
@@ -0,0 +1,50 @@
+// var num =10;
+// num =30;
+// console.log("before block scope",num);
+
+// {
+//     var num =20;
+//     console.log("after block scope",num);
+
+// }
+//  console.log("after block scope",num);
+
+// let num =10;
+
+// console.log("before block scope",num);
+
+// {
+//     let num =20;
+//     console.log("after block scope",num);
+
+// }
+//  console.log("after block scope",num);
+
+
+
+// let num = 999999999999;
+// console.log(num)
+// console.log(typeof num)
+
+// console.log(Number.MAX_SAFE_INTEGER);
+
+// let num =9999999999999;
+// console.log(num)
+
+//  num = BigInt("77777777777777777");
+//  console.log(num);
+//  console.log(typeof num)
+
+// const user ={
+//     username : "rohit sharma",
+//     age : 39,
+//     isloggedin: true
+// }
+
+// console.log(user)
+
+// let acTemp =null;
+// console.log(acTemp);
+
+// console.log(typeof acTemp)
+
